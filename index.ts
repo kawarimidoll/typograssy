890037fa7042819ec14e045e4472bb099f7394f3
kawarimidoll <kawarimidoll+git@gmail.com>
2021-02-08 18:22:50 +0900

    add first svg response

--- index.ts
+++ index.ts
@@ -7,10 +7,13 @@ const server = serve({ port: 8080 });
 console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
 
 const requestHandler = (req: ServerRequest) => {
-  const bodyContent = "Your user-agent is:\n\n" +
-    (req.headers.get("user-agent") || "Unknown");
+  const headers = new Headers({ "Content-Type": "image/svg+xml" });
+  const body =
+    `<svg width="600" height="400" xmlns="http://www.w3.org/2000/svg">
+  <rect x="10" y="10" width="300" height="100" fill="#830"></rect>
+  </svg>`;
 
-  req.respond({ status: 200, body: bodyContent });
+  req.respond({ status: 200, headers, body });
 };
 
 for await (const request of server) {
